b02a3d5e5cba4ce90d5a8802c884ba321e357064
Jon Goodnow <goodnow.jonathan@gmail.com>
2014-11-15 16:54:58 -0500

    some things for a server we might use later

--- server/app.py
+++ server/app.py
@@ -1,14 +1,44 @@
-#! /usr/bin/env python
-
+import logging
+import tornado.auth
+import tornado.escape
 import tornado.ioloop
 import tornado.web
-import server.routes as rt
+import os.path
+import uuid
 
-application = tornado.web.Application([
-    (r"/", rt.MainHandler),
-])
+from tornado.concurrent import Future
+from tornado import gen
+from tornado.options import define, options, parse_command_line
+
+import routes
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
+
+# Making this a non-singleton is left as an exercise for the reader.
+global_message_buffer = routes.MessageBuffer()
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", routes.MainHandler),
+            (r"/auth/login", routes.AuthLoginHandler),
+            (r"/auth/logout", routes.AuthLogoutHandler),
+            (r"/a/message/new", routes.MessageNewHandler),
+            (r"/a/message/updates", routes.MessageUpdatesHandler),
+            ],
+        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        login_url="/auth/login",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+        )
+    app.listen(options.port)
+    print("Listening on Port 8888")
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
-    application.listen(8888)
-    print("Listening on port 8888")
-    tornado.ioloop.IOLoop.instance().start()
+    main()
