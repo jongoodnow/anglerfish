bfd00c08206e2eb2f59c7db476a3d40c75ce2117
Jon Goodnow <goodnow.jonathan@gmail.com>
2014-11-16 04:10:23 -0500

    calculated vector endpoint

--- src/kinect/app.py
+++ src/kinect/app.py
@@ -1,9 +1,11 @@
+from __future__ import division
 from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 import cv
 import numpy as np
 from Tkinter import *
 import Image, ImageTk
 import time
+import urllib2
 
 def save_img():
     global rgb
@@ -83,10 +85,10 @@ def main():
     upper_bound = np.amax(corners_arr, axis=0)
     while True:
         (depth,_) = get_depth()
-        depth_array = np.array(depth)
+        depth_array = np.array(depth).astype(np.uint8)
         d3 = np.array([y[lower_bound[0]:upper_bound[0]] 
             for y in depth_array[lower_bound[1]:upper_bound[1]]])
-        d3 = d3.astype(np.uint8)
+        d3 = d3
         depths_per_x = d3.mean(axis=0) - bg_depth
         depths_per_x = np.array([i if i < -3.5 else 0 for i in depths_per_x])
         minimum = np.amin(depths_per_x)
@@ -102,16 +104,29 @@ def main():
                     spanright = pos
                 if i == minimum:
                     bodyx = pos
-            lefty = yposition(spanleft)
             righty = yposition(spanright)
             bodyy = yposition(bodyx)
-            leftz = depth_array[spanleft][lefty]
             rightz = depth_array[spanright][righty]
             bodyz = depth_array[bodyx][bodyy]
-            print "left (", spanleft, lefty, leftz, ")"
-            print "right (", spanright, righty, rightz, ")"
-            print "body (", bodyx, bodyy, bodyz, ")"
-
+            pointz = None
+            if spanleft < bodyx - 29:
+                pointx = spanleft
+                pointy = yposition(spanleft)
+                pointz = depth_array[spanleft][pointy]
+            elif spanright > bodyx + 29:
+                pointx = spanright
+                pointy = yposition(spanright)
+                pointz = depth_array[spanright][pointy]
+            if pointz is not None and pointz < bodyz and pointy != bodyy and pointx != bodyx:
+                xzslope = (pointz - bodyz) / (pointx - bodyx)
+                yzslope = (pointz - bodyz) / (pointy - bodyy)
+                zdiff = bg_depth - bodyz
+                xdist = zdiff / xzslope
+                ydist = zdiff / yzslope
+                xcoord = (xdist + bodyx) / upper_bound[1]
+                ycoord = (ydist + bodyy) / upper_bound[0]
+                urllib2.urlopen(
+                    "http://129.161.90.185:8888/update?pointer=%f,%f" %(xcoord, ycoord)).read()
         plt.clf()
         plt.scatter(np.arange(depths_per_x.size), depths_per_x)
         plt.ylim([-255, 0])
