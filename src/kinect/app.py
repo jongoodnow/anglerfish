4c1194a318c1dba0447030f6e62da0fd69ca7a7b
Jon Goodnow <goodnow.jonathan@gmail.com>
2014-11-15 23:28:46 -0500

    graph of depth per vertical cross section

--- src/kinect/app.py
+++ src/kinect/app.py
@@ -3,17 +3,12 @@ import cv
 import numpy as np
 from Tkinter import *
 import Image, ImageTk
+import time
 
 def save_img():
     global rgb
     (rgb,_) = get_video()
     cv.SaveImage('/tmp/anglerfish-img.jpg', cv.fromarray(np.array(rgb)))
-
-
-def save_depth():
-    global depth
-    (depth,) = get_depth()
-    cv.SaveImage('/tmp/anglerfish-depth.jpg', cv.fromarray(np.array(depth)))
 
 
 def get_corners():
@@ -47,14 +42,39 @@ def get_corners():
 
 
 def get_bg_depth():
+    global corners
     corners = get_corners()
     mtx = np.matrix(corners)
     mean = mtx.mean(0)
     middle_coords = (int(mean.item((0, 0))), int(mean.item((0, 1))))
-    #save_depth()
-    #im = Image.open('/tmp/anglerfish-depth.jpg')
-    #pix = im.load()
-    #return pix[middle_coords[0]][middle_coords[1]]
+    global depth
     (depth,_) = get_depth()
-    d3 = np.dstack((depth,depth,depth)).astype(np.uint8)
+    d3 = np.dstack((depth, depth, depth)).astype(np.uint8)
     return d3[middle_coords[0]][middle_coords[1]][0]
+
+
+def main():
+    import matplotlib.pyplot as plt
+    bg_depth = get_bg_depth()
+    plt.ion()
+    plt.show()
+    while True:
+        (depth,_) = get_depth()
+        d3 = np.array(depth) #np.dstack((depth, depth, depth)).astype(np.uint8)
+        corners_arr = np.array(corners)
+        lower_bound = np.amin(corners_arr, axis=0)
+        upper_bound = np.amax(corners_arr, axis=1)
+        d3 = d3[lower_bound[0]:upper_bound[0], lower_bound[1]:upper_bound[1]]
+        #fixed_depth = np.subtract(d3[:][:, 0], bg_depth)
+        depths_per_x = d3.mean(axis=0)
+        #cv.ShowImage('both',cv.fromarray(d3))
+        #cv.WaitKey(5)
+        #depth_hist = np.histogram(fixed_depth, bins=np.arange(320))
+        plt.clf()
+        plt.plot(np.arange(depths_per_x.size), depths_per_x)
+        plt.draw()
+    
+
+
+if __name__ == '__main__':
+    main()
